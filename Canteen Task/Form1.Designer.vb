0bb32c5090c27611ddb22322b4abc0b3848ce62a
PI_Gorbo <sam.gorbwork@gmail.com>
2019-03-14 21:00:27 +1100

    1. Added a "Add User" Fucntion into the AI 2. Changed List box font for better reading abiltiy 3. Created a function that automatically Updates the File when a change is made to the Array

--- Canteen Task/Form1.Designer.vb
+++ Canteen Task/Form1.Designer.vb
@@ -35,8 +35,15 @@ Partial Class Form1
         Me.Lstbx = New System.Windows.Forms.ListBox()
         Me.Btn_Buy = New System.Windows.Forms.Button()
         Me.Btn_Add = New System.Windows.Forms.Button()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.__ = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.TxtBx_AddUser_StartBal = New System.Windows.Forms.TextBox()
+        Me.Txtbx_AddUser_Name = New System.Windows.Forms.TextBox()
+        Me.Btn_AddUser = New System.Windows.Forms.Button()
         Me.GroupBox1.SuspendLayout()
         Me.GroupBox2.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
         Me.SuspendLayout()
         '
         'GroupBox1
@@ -45,42 +52,48 @@ Partial Class Form1
         Me.GroupBox1.Controls.Add(Me.Txt_Name_Buy)
         Me.GroupBox1.Controls.Add(Me.Label2)
         Me.GroupBox1.Controls.Add(Me.Label3)
-        Me.GroupBox1.Location = New System.Drawing.Point(56, 63)
+        Me.GroupBox1.Location = New System.Drawing.Point(42, 51)
+        Me.GroupBox1.Margin = New System.Windows.Forms.Padding(2)
         Me.GroupBox1.Name = "GroupBox1"
-        Me.GroupBox1.Size = New System.Drawing.Size(213, 160)
+        Me.GroupBox1.Padding = New System.Windows.Forms.Padding(2)
+        Me.GroupBox1.Size = New System.Drawing.Size(160, 130)
         Me.GroupBox1.TabIndex = 0
         Me.GroupBox1.TabStop = False
         Me.GroupBox1.Text = "Buy Food"
         '
         'Txt_Total_Buy
         '
-        Me.Txt_Total_Buy.Location = New System.Drawing.Point(82, 113)
+        Me.Txt_Total_Buy.Location = New System.Drawing.Point(62, 92)
+        Me.Txt_Total_Buy.Margin = New System.Windows.Forms.Padding(2)
         Me.Txt_Total_Buy.Name = "Txt_Total_Buy"
-        Me.Txt_Total_Buy.Size = New System.Drawing.Size(100, 22)
+        Me.Txt_Total_Buy.Size = New System.Drawing.Size(76, 20)
         Me.Txt_Total_Buy.TabIndex = 4
         '
         'Txt_Name_Buy
         '
-        Me.Txt_Name_Buy.Location = New System.Drawing.Point(82, 34)
+        Me.Txt_Name_Buy.Location = New System.Drawing.Point(62, 28)
+        Me.Txt_Name_Buy.Margin = New System.Windows.Forms.Padding(2)
         Me.Txt_Name_Buy.Name = "Txt_Name_Buy"
-        Me.Txt_Name_Buy.Size = New System.Drawing.Size(100, 22)
+        Me.Txt_Name_Buy.Size = New System.Drawing.Size(76, 20)
         Me.Txt_Name_Buy.TabIndex = 5
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(16, 118)
+        Me.Label2.Location = New System.Drawing.Point(12, 96)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(40, 17)
+        Me.Label2.Size = New System.Drawing.Size(31, 13)
         Me.Label2.TabIndex = 1
         Me.Label2.Text = "Total"
         '
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(16, 34)
+        Me.Label3.Location = New System.Drawing.Point(12, 28)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(45, 17)
+        Me.Label3.Size = New System.Drawing.Size(35, 13)
         Me.Label3.TabIndex = 2
         Me.Label3.Text = "Name"
         '
@@ -90,88 +103,159 @@ Partial Class Form1
         Me.GroupBox2.Controls.Add(Me.Txt_Name_Money)
         Me.GroupBox2.Controls.Add(Me.Label1)
         Me.GroupBox2.Controls.Add(Me.Label4)
-        Me.GroupBox2.Location = New System.Drawing.Point(333, 63)
+        Me.GroupBox2.Location = New System.Drawing.Point(250, 51)
+        Me.GroupBox2.Margin = New System.Windows.Forms.Padding(2)
         Me.GroupBox2.Name = "GroupBox2"
-        Me.GroupBox2.Size = New System.Drawing.Size(213, 160)
+        Me.GroupBox2.Padding = New System.Windows.Forms.Padding(2)
+        Me.GroupBox2.Size = New System.Drawing.Size(160, 130)
         Me.GroupBox2.TabIndex = 6
         Me.GroupBox2.TabStop = False
         Me.GroupBox2.Text = "Add Money"
         '
         'Txt_Amount_Money
         '
-        Me.Txt_Amount_Money.Location = New System.Drawing.Point(82, 113)
+        Me.Txt_Amount_Money.Location = New System.Drawing.Point(62, 92)
+        Me.Txt_Amount_Money.Margin = New System.Windows.Forms.Padding(2)
         Me.Txt_Amount_Money.Name = "Txt_Amount_Money"
-        Me.Txt_Amount_Money.Size = New System.Drawing.Size(100, 22)
+        Me.Txt_Amount_Money.Size = New System.Drawing.Size(76, 20)
         Me.Txt_Amount_Money.TabIndex = 4
         '
         'Txt_Name_Money
         '
-        Me.Txt_Name_Money.Location = New System.Drawing.Point(82, 34)
+        Me.Txt_Name_Money.Location = New System.Drawing.Point(62, 28)
+        Me.Txt_Name_Money.Margin = New System.Windows.Forms.Padding(2)
         Me.Txt_Name_Money.Name = "Txt_Name_Money"
-        Me.Txt_Name_Money.Size = New System.Drawing.Size(100, 22)
+        Me.Txt_Name_Money.Size = New System.Drawing.Size(76, 20)
         Me.Txt_Name_Money.TabIndex = 5
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(16, 118)
+        Me.Label1.Location = New System.Drawing.Point(12, 96)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(56, 17)
+        Me.Label1.Size = New System.Drawing.Size(43, 13)
         Me.Label1.TabIndex = 1
         Me.Label1.Text = "Amount"
         '
         'Label4
         '
         Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(16, 34)
+        Me.Label4.Location = New System.Drawing.Point(12, 28)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(45, 17)
+        Me.Label4.Size = New System.Drawing.Size(35, 13)
         Me.Label4.TabIndex = 2
         Me.Label4.Text = "Name"
         '
         'Lstbx
         '
+        Me.Lstbx.BackColor = System.Drawing.SystemColors.HighlightText
+        Me.Lstbx.Cursor = System.Windows.Forms.Cursors.Default
+        Me.Lstbx.Font = New System.Drawing.Font("Bahnschrift Condensed", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Lstbx.FormattingEnabled = True
-        Me.Lstbx.ItemHeight = 16
-        Me.Lstbx.Location = New System.Drawing.Point(579, 12)
+        Me.Lstbx.ItemHeight = 18
+        Me.Lstbx.Location = New System.Drawing.Point(434, 10)
+        Me.Lstbx.Margin = New System.Windows.Forms.Padding(2)
         Me.Lstbx.Name = "Lstbx"
-        Me.Lstbx.Size = New System.Drawing.Size(431, 548)
+        Me.Lstbx.Size = New System.Drawing.Size(324, 436)
         Me.Lstbx.TabIndex = 7
         '
         'Btn_Buy
         '
-        Me.Btn_Buy.Location = New System.Drawing.Point(106, 256)
+        Me.Btn_Buy.Location = New System.Drawing.Point(80, 208)
+        Me.Btn_Buy.Margin = New System.Windows.Forms.Padding(2)
         Me.Btn_Buy.Name = "Btn_Buy"
-        Me.Btn_Buy.Size = New System.Drawing.Size(75, 23)
+        Me.Btn_Buy.Size = New System.Drawing.Size(56, 26)
         Me.Btn_Buy.TabIndex = 8
         Me.Btn_Buy.Text = "Buy"
         Me.Btn_Buy.UseVisualStyleBackColor = True
         '
         'Btn_Add
         '
-        Me.Btn_Add.Location = New System.Drawing.Point(398, 256)
+        Me.Btn_Add.Location = New System.Drawing.Point(298, 208)
+        Me.Btn_Add.Margin = New System.Windows.Forms.Padding(2)
         Me.Btn_Add.Name = "Btn_Add"
-        Me.Btn_Add.Size = New System.Drawing.Size(75, 23)
+        Me.Btn_Add.Size = New System.Drawing.Size(56, 26)
         Me.Btn_Add.TabIndex = 9
         Me.Btn_Add.Text = "Add"
         Me.Btn_Add.UseVisualStyleBackColor = True
         '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.Btn_AddUser)
+        Me.GroupBox3.Controls.Add(Me.Txtbx_AddUser_Name)
+        Me.GroupBox3.Controls.Add(Me.TxtBx_AddUser_StartBal)
+        Me.GroupBox3.Controls.Add(Me.Label6)
+        Me.GroupBox3.Controls.Add(Me.__)
+        Me.GroupBox3.Location = New System.Drawing.Point(42, 258)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(368, 198)
+        Me.GroupBox3.TabIndex = 10
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "Add User"
+        '
+        '__
+        '
+        Me.__.AutoSize = True
+        Me.__.Location = New System.Drawing.Point(46, 59)
+        Me.__.Name = "__"
+        Me.__.Size = New System.Drawing.Size(35, 13)
+        Me.__.TabIndex = 0
+        Me.__.Text = "Name"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(46, 122)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(85, 13)
+        Me.Label6.TabIndex = 1
+        Me.Label6.Text = "Starting Balance"
+        '
+        'TxtBx_AddUser_StartBal
+        '
+        Me.TxtBx_AddUser_StartBal.Location = New System.Drawing.Point(178, 122)
+        Me.TxtBx_AddUser_StartBal.Name = "TxtBx_AddUser_StartBal"
+        Me.TxtBx_AddUser_StartBal.Size = New System.Drawing.Size(100, 20)
+        Me.TxtBx_AddUser_StartBal.TabIndex = 2
+        '
+        'Txtbx_AddUser_Name
+        '
+        Me.Txtbx_AddUser_Name.Location = New System.Drawing.Point(178, 56)
+        Me.Txtbx_AddUser_Name.Name = "Txtbx_AddUser_Name"
+        Me.Txtbx_AddUser_Name.Size = New System.Drawing.Size(100, 20)
+        Me.Txtbx_AddUser_Name.TabIndex = 3
+        '
+        'Btn_AddUser
+        '
+        Me.Btn_AddUser.Location = New System.Drawing.Point(121, 160)
+        Me.Btn_AddUser.Name = "Btn_AddUser"
+        Me.Btn_AddUser.Size = New System.Drawing.Size(75, 23)
+        Me.Btn_AddUser.TabIndex = 4
+        Me.Btn_AddUser.Text = "Add User"
+        Me.Btn_AddUser.UseVisualStyleBackColor = True
+        '
         'Form1
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1022, 581)
+        Me.ClientSize = New System.Drawing.Size(766, 472)
+        Me.Controls.Add(Me.GroupBox3)
         Me.Controls.Add(Me.Btn_Add)
         Me.Controls.Add(Me.Btn_Buy)
         Me.Controls.Add(Me.Lstbx)
         Me.Controls.Add(Me.GroupBox2)
         Me.Controls.Add(Me.GroupBox1)
+        Me.Margin = New System.Windows.Forms.Padding(2)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.GroupBox1.ResumeLayout(False)
         Me.GroupBox1.PerformLayout()
         Me.GroupBox2.ResumeLayout(False)
         Me.GroupBox2.PerformLayout()
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
         Me.ResumeLayout(False)
 
     End Sub
@@ -189,4 +273,10 @@ Partial Class Form1
     Friend WithEvents Lstbx As ListBox
     Friend WithEvents Btn_Buy As Button
     Friend WithEvents Btn_Add As Button
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents Btn_AddUser As Button
+    Friend WithEvents Txtbx_AddUser_Name As TextBox
+    Friend WithEvents TxtBx_AddUser_StartBal As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents __ As Label
 End Class
--- Canteen Task/Form1.vb
+++ Canteen Task/Form1.vb
@@ -1,4 +1,5 @@
 ﻿Public Class Form1
+
     Structure Clients
         Dim name As String
         Dim balance As Single
@@ -6,19 +7,6 @@
 
     Dim student(30) As Clients
     Dim count As Int16 = 0
-
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        count = 0
-        FileSystem.FileOpen(1, "Canteen.txt", OpenMode.Input)
-        While Not EOF(1)
-            student(count).name = FileSystem.LineInput(1).ToLower()
-            student(count).balance = Convert.ToSingle(FileSystem.LineInput(1))
-            count += 1
-        End While
-        FileSystem.FileClose(1)
-        count -= 1
-        Display()
-    End Sub
 
     Private Sub Display()
         Lstbx.Items.Clear()
@@ -28,22 +16,95 @@
         Next
     End Sub
 
+    Private Sub Array_To_File()
+        FileSystem.FileOpen(1, "Canteen.txt", OpenMode.Output)
+        For i = 0 To count
+            FileSystem.PrintLine(1, student(i).name)
+            FileSystem.WriteLine(1, student(i).balance)
+        Next
+        FileSystem.FileClose()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        count = 0
+        FileSystem.FileOpen(1, "Canteen.txt", OpenMode.Input)
+        While Not EOF(1)
+            student(count).name = StrConv(FileSystem.LineInput(1), vbProperCase)
+            student(count).balance = Convert.ToSingle(FileSystem.LineInput(1))
+            count += 1
+        End While
+        FileSystem.FileClose(1)
+        count -= 1
+        Display()
+    End Sub
+
     Private Sub Btn_Buy_Click(sender As Object, e As EventArgs) Handles Btn_Buy.Click
 
-        Dim name As String = Txt_Name_Buy.Text.ToLower()
-        Dim amount As Single = Convert.ToSingle(Txt_Total_Buy.Text)
+        Dim name As String = StrConv(Txt_Name_Buy.Text, vbProperCase)
+        Dim amount As Single = StrConv(Convert.ToSingle(Txt_Total_Buy.Text), vbProperCase)
+        Dim flag As Boolean = False
 
         For i = 0 To count
             If name = student(i).name Then
                 If (student(i).balance - amount) < 0 Then
                     MsgBox("Cannot Afford Item")
+                    flag = True
                 Else
                     student(i).balance -= amount
+                    Display()
+                    MsgBox("Purchase sucessful!")
+                    Array_To_File()
+                    flag = True
                 End If
                 Exit For
             End If
         Next
-        Display()
+        If flag = False Then
+            MsgBox("ERROR: User not registered.")
+        End If
+    End Sub
+
+    Private Sub Btn_Add_Click(sender As Object, e As EventArgs) Handles Btn_Add.Click
+
+        Dim name As String = StrConv(Txt_Name_Money.Text, vbProperCase)
+        Dim amount As Single = StrConv(Convert.ToSingle(Txt_Amount_Money.Text), vbProperCase)
+        Dim flag As Boolean = False
+
+        For i = 0 To count
+            If name = student(i).name Then
+                student(i).balance += amount
+                flag = True
+                Display()
+                MsgBox("Amount added to account!")
+                Array_To_File()
+                Exit For
+            End If
+        Next
+        If flag = False Then
+            MsgBox("ERROR: User not registered.")
+        End If
+    End Sub
+
+    Private Sub Btn_AddUser_Click(sender As Object, e As EventArgs) Handles Btn_AddUser.Click
+
+        Dim name As String = StrConv(Txtbx_AddUser_Name.Text, vbProperCase)
+        Dim amount As Single = StrConv(Convert.ToSingle(TxtBx_AddUser_StartBal.Text), vbProperCase)
+        Dim flag As Boolean = False
+
+        For i = 0 To count
+            If name = student(i).name Then
+                MsgBox("User is already added.")
+                flag = True
+            End If
+        Next
+        If flag = False Then
+            count += 1
+            student(count).name = name
+            student(count).balance = amount
+            Display()
+            Array_To_File()
+            MsgBox("User sucessfully added!")
+        End If
 
     End Sub
 End Class
